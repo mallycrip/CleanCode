65d43b28dc8493e140d939a2bccefc0eeff3486e
Min ki - Son <migsking@naver.com>
2019-04-17 08:33:14 +0900

    Update magicmethod.py

--- 4M13D/magicmethod.py
+++ 4M13D/magicmethod.py
@@ -2,6 +2,12 @@
 a = int(input())
 print(type(a))
 print(type(a.__str__())) # str(obj) -> obg.__str__()
+
+객체 생성 -> __init__()
+print() -> __repr__()
+len() -> __len__()
+str() -> __str__()
+
 '''
 
 class cls:
@@ -15,7 +21,8 @@ class cls:
     def __str__(self):
         return str(self._a) + str(self._b)
 
+    
 b = cls(1,3)
 
-print(b)
-print(str(b))
+print(b)    #print 를 사용시 __repr__ 
+print(str(b)) # str 사용시 __str__
