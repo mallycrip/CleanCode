aca6d1163954be3e5bfa7e3732f85b902f86540b
migsking <migsking@naver.com>
2019-04-26 19:11:59 +0900

    .
    .

--- 4M19D/Login.py
+++ 4M19D/Login.py
@@ -4,35 +4,43 @@ app = Flask(__name__)
 
 user = []
 
-@app.route('/', methods=['POST'])
+@app.route('/', methods=['GET'])
 def register():
-    id = request.json["id"]
-    pw = request.json["pw"]
+    user_id = request.json["id"]
+    user_pw = request.json["pw"]
+    register_type_error = Response("Be not a str.", 403)
+    register_error = Response("Already registered ID",409)
+    register_success = Response("Register Success : " + user_id, 200)
 
-    user.append({
-        'id':id,
-        'pw':pw
-    })
+    if type(user_id) == str\
+            and type(user_pw) == str:
+        for register in user:
+            if register['id'] == user_id:
+                return register_error
+        else:
+            user.append({
+                'id': user_id,
+                'pw': user_pw
+            })
+            print("register success", user_id)
+            return register_success
+    else:
+        return register_type_error
 
-    response = Response("Register Success | id : "+id)
-    print("Register Success", id)
-    print(user)
-    return response
 
 @app.route('/login', methods=['POST'])
 def login():
     login_id = request.json["id"]
     login_pw = request.json["pw"]
+    login_success = Response("Hello "+login_id, 200)
+    login_failed = Response("Login failed", 401)
 
-    for a in user:
-        print(a)
-        if a['id'] == login_id:
-            if a['pw'] == login_pw:
-                print("Login Success")
-                return "LOGIN SUCCESS"
-            else:
-                return "Check your pw."
-    return "Check your ID"
+    for login in user:
+        if login['id'] == login_id \
+                and login['pw'] == login_pw:
+            print("Login Success")
+            return login_success
+    return login_failed
 
 if __name__ == '__main__':
     app.run(debug=True)
